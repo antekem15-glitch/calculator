24c3a33cbaf84c043336adb847296326a734f08b
Ante <241960689+antekem15-glitch@users.noreply.github.com.>
2026-06-03 09:44:52 +0200

    add design. add functions for number input

---
+++ myscript.js
@@ -0,0 +1,53 @@
+
+const display = document.getElementById("display")
+
+let firstValue = null;
+let secondValue = null;
+let aktuellerOperator = null;
+
+const zero=document.getElementById('zero').addEventListener("click", function() { display.value += 0;
+});
+const one=document.getElementById('one').addEventListener("click", function() {  display.value += 1;
+});
+const two=document.getElementById('two').addEventListener("click", function() { display.value += 2;
+});
+const three=document.getElementById('three').addEventListener("click", function() { display.value += 3;
+});
+const four=document.getElementById('four').addEventListener("click", function() { display.value += 4;
+});
+const five=document.getElementById('five').addEventListener("click", function() { display.value += 5;
+});
+const six=document.getElementById('six').addEventListener("click", function() { display.value += 6;
+});
+const seven=document.getElementById('seven').addEventListener("click", function() { display.value += 7;
+});
+const eight=document.getElementById('eight').addEventListener("click", function() { display.value += 8;
+});
+const nine=document.getElementById('nine').addEventListener("click", function() { display.value += 9;
+});
+const plus=document.getElementById('plus').addEventListener("click", function() { display.value = "+";
+});
+const minus=document.getElementById('minus').addEventListener("click", function() { display.value = "-";
+});
+const del =document.getElementById('delete').addEventListener("click", function() { display.value = " "; 
+});
+const dot=document.getElementById('dot').addEventListener("click", function() {display.value += ".";
+});
+
+const clear = document.getElementById('delete').addEventListener("click", function() { 
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
